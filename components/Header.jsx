82b3260f61cebed5aeccae48c184465726ff27ba
kievzenit <kir.backender@gmail.com>
2022-10-04 13:46:17 +0300

    Active link support to Header added

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,19 +1,30 @@
-import Link from "next/link";
+import Link from "next/link"
+import cn from "classnames"
 
 import styles from "../styles/Header.module.css"
 
-export default function Header () {
+export default function Header({activeLink}) {
     return (
         <header className={styles.header}>
             <Link href="/">
-                <a className={styles.logo}>kievzenit</a>
+                <a className={
+                        activeLink === "home" ? 
+                            `${styles.logo} ${styles.active}` : `${styles.logo}`}>kievzenit</a>
             </Link>
             <nav className={styles.nav}>
                 <Link href="about">
-                    <a className={styles.link}>about</a>
+                    <a className={
+                        activeLink === "about" ? 
+                            `${styles.link} ${styles.active}` : `${styles.link}`}>
+                        about
+                    </a>
                 </Link>
                 <Link href="projects">
-                    <a className={styles.link}>projects</a>
+                    <a className={
+                        activeLink === "projects" ? 
+                            `${styles.link} ${styles.active}` : `${styles.link}`}>
+                            projects
+                    </a>
                 </Link>
             </nav>
         </header>
