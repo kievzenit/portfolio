c78737f87a55855ed127044ffa6792202bce5014
kievzenit <kir.backender@gmail.com>
2022-10-07 14:57:08 +0300

    AboutMain component added

---
+++ components/AboutMain.jsx
@@ -0,0 +1,27 @@
+import styles from "../styles/AboutMain.module.css"
+
+export default function AboutMain() {
+    return (
+        <main className={styles.container}>
+            <div className={styles["inner-container"]}>
+                <h1>Who am I?</h1>
+                <section className={styles.section}>I am Ukrainian web developer, passionate about everything related to computers. Focused on improving my profesional skills.</section>
+                <br/>
+                <section className={styles.section}>
+                    <p>My tech skills:</p>
+                    <ul>
+                        <li>C#/.NET (ASP.NET, clean architecture, CQRS)</li>
+                        <li>JavaScript (React/Next)</li>
+                        <li>HTML5/CSS3/vanilla js</li>
+                        <li>Python (as scripting language)</li>
+                        <li>Sql Server</li>
+                        <li>PostgreSQL</li>
+                        <li>Redis</li>
+                        <li>Assembler (x86-64)</li>
+                        <li>Golang (a little bit)</li>
+                    </ul>
+                </section>
+            </div>
+        </main>
+    )
+}
--- pages/about/index.js
+++ pages/about/index.js
@@ -1,9 +1,12 @@
+import AboutMain from "../../components/AboutMain";
 import Layout from "../../components/Layout";
 
 export default function About() {
   const activeLink = "about"
 
   return (
-    <Layout activeLink={activeLink}></Layout>
+    <Layout activeLink={activeLink}>
+      <AboutMain/>
+    </Layout>
   )
 }
